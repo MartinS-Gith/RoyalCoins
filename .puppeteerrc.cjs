d2d4e69576c2e2546e41da67b21620da3fb89490
DkMonki <martin.simon.canales@gmail.com>
2026-02-10 19:53:47 -0300

    Cambio Pagina Completa

--- .puppeteerrc.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require('path');
-
-/**
- * @type {import("puppeteer").Configuration}
- */
-module.exports = {
-  // Esto obliga a Puppeteer a instalar y buscar Chrome en la misma carpeta
-  cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
-};
